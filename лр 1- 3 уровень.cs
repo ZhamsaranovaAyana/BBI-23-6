5134d92633b5a4adbf7522c3521f23a57a64f5f2
ZhamsaranovaAyana <146060652+ZhamsaranovaAyana@users.noreply.github.com>
2023-10-19 21:41:37 +0200

    Update лр 1- 3 уровень.cs
    изменила до х=1, результат вроде выходит..

--- лр 1- 3 уровень.cs
+++ лр 1- 3 уровень.cs
@@ -1,37 +1,38 @@
-﻿using System;
+using System;
+
 class Program
 {
-    public static void Main()
+    static void Main(string[] args)
     {
-        double sum = 0;
-        double a = 0.1;
-        double b = 1;
-        double h = 0.1;
-        int i = 0;
-        for (double x = a; x <= b; x += h, i++)
+        double a = 0.1, b = 1, h = 0.1;
+        for (double x = a; x <= b; x += h)
         {
-            double cur;
-            long fact = 1;
-            double y = Math.Cos(x);
-            for (int g = 2; g <= 2 * i; g++)
+            double s = 0;
+            for (int i = 0; ; i++)
             {
-                fact = fact * g;
+                double k = Math.Pow(-1, i) * (Math.Pow(x, 2 * i) / fac(2 * i));
+                if (Math.Abs(k) < 0.0001)
+                {
+                    break;
+                }
+                s += k;
             }
-            if (i % 2 == 0)
-            {
-                cur = Math.Pow(x, 2 * i) / fact;
-            }
-            else
-            {
-                cur = -1 * Math.Pow(x, 2 * i) / fact;
-            }
-            if (Math.Abs(cur) < 0.0001)
-            {
-                break;
-            }
-            sum += cur;
-            Console.WriteLine($"i = {i}, x = {x}, current element = {cur}, cos(x) = {y} \n\n");
+            Console.WriteLine("x = {0}, s = {1}, y = {2}", x, s, y(x));
         }
-        Console.WriteLine($"sum = {sum}");
     }
-};
+
+    static double y(double x)
+    {
+        return Math.Cos(x);
+    }
+
+    static double fac(int n)
+    {
+        double result = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+}
