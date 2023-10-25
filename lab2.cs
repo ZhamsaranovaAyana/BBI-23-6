053b4fa788d779d5b813e6b3f61b5e05cb4f2e69
ZhamsaranovaAyana <146060652+ZhamsaranovaAyana@users.noreply.github.com>
2023-10-25 21:23:58 +0300

    Add files via upload
    лабораторная 2 попытка 1

---
+++ lab2.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace аяна_сделала
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.3
+
+            //int a = Int32.Parse(Console.ReadLine());
+            //int b = Int32.Parse(Console.ReadLine());
+            //int c;
+            //if (a > 0)
+            //{
+            //    c = Math.Max(a, b);
+            //}
+            //else
+            //{
+            //    c = Math.Min(a, b);
+            //}
+            //Console.WriteLine($"c = {c}");
+
+
+
+
+            //1.6(1)
+            //double r = 3.2;
+            //double s = 3.5;
+
+            //double circleArea = Math.PI * Math.Pow(r, 2);
+            //double squareArea = Math.Pow(s, 2);
+
+            //if (circleArea <= squareArea)
+            //{
+            //    Console.WriteLine("Круг помещается в квадрате");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Круг не помещается в квадрате");
+            //}
+
+            //1.6(2)
+            //double r = 3.2;
+            //double s = 4;
+
+            //double circleArea = Math.PI * Math.Pow(r, 2);
+            //double squareArea = Math.Pow(s, 2);
+
+            //if (circleArea <= squareArea)
+            //{
+            //    Console.WriteLine("Круг помещается в квадрате");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Круг не помещается в квадрате");
+            //}
+
+            //1.6(3)
+            //double r = 6;
+            //double s = 9;
+
+            //double circleArea = Math.PI * Math.Pow(r, 2);
+            //double squareArea = Math.Pow(s, 2);
+
+            //if (circleArea <= squareArea)
+            //{
+            //    Console.WriteLine("Круг помещается в квадрате");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Круг не помещается в квадрате");
+            //}
+
+            //1.9
+            //Console.Write("Введите значение аргумента x: ");
+            //double x = double.Parse(Console.ReadLine());
+
+            //double y;
+
+            //if (x <= -1)
+            //{
+            //    y = 0;
+            //}
+            //else if (x <= 0)
+            //{
+            //    y = 1 + x;
+            //}
+            //else
+            //{
+            //    y = 1;
+            //}
+
+            //Console.WriteLine("Значение функции y = " + y);
+
+            //2.3.
+            //int n; // количество учеников в классе
+            //double weight; // вес ученика в кг
+
+            //Console.Write("введите количество учеников в классе: ");
+            //n = Convert.ToInt32(Console.ReadLine());
+
+            //double totalMilk = 0; // общее количество молока
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    Console.Write($"введите вес {i}-го ученика (кг): ");
+            //    weight = Convert.ToDouble(Console.ReadLine());
+
+            //    if (weight < 30)
+            //    {
+            //        // тогда дополнительно выдается стакан молока (0.2 л)
+            //        totalMilk += 0.2; // 0.2 литра молока
+            //    }
+            //}
+
+            //Console.WriteLine($"каждый день для одного класса из {n} учеников нужно {totalMilk} литров молока.");
+            //Console.ReadLine();
+
+            //2.6
+            //Console.WriteLine("Введите количество точек:");
+            //int n = int.Parse(Console.ReadLine());
+
+            //int count = 0;
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.WriteLine($"Введите координаты точки {i + 1}:");
+            //    double x = double.Parse(Console.ReadLine());
+            //    double y = double.Parse(Console.ReadLine());
+
+            //    if (x >= 0 && x <= Math.PI && y >= 0 && y <= Math.Sin(x))
+            //    {
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine($"Количество точек, принадлежащих фигуре: {count}");
+
+
+            //2.9
+            //int n = Int32.Parse(Console.ReadLine());
+            //int best = 0;
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    int dist = Int32.Parse(Console.ReadLine());
+            //    best = Math.Max(best, dist);
+            //}
+            //
+            //Console.WriteLine($"Лучший результат: {best}");
+            //1.3  
+            //int a = Int32.Parse(Console.ReadLine());
+            //int b = Int32.Parse(Console.ReadLine());
+            //int c;
+            //if (a > 0)
+            //{
+            //    c = Math.Max(a, b);
+            //}
+            //else
+            //{
+            //    c = Math.Min(a, b);
+            //}
+            //Console.WriteLine($"c = {c}");
+
+
+            //1.6(1)
+            //double r = 3.2;
+            //double s = 3.5;
+
+            //double circleArea = Math.PI * Math.Pow(r, 2);
+            //double squareArea = Math.Pow(s, 2);
+
+            //if (circleArea <= squareArea)
+            //{
+            //    Console.WriteLine("Круг помещается в квадрате");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Круг не помещается в квадрате");
+            //}
+
+            //1.9
+            //Console.Write("Введите значение аргумента x: ");
+            //double x = double.Parse(Console.ReadLine());
+
+            //double y;
+
+            //if (x <= -1)
+            //{
+            //    y = 0;
+            //}
+            //else if (x <= 0)
+            //{
+            //    y = 1 + x;
+            //}
+            //else
+            //{
+            //    y = 1;
+            //}
+
+            //Console.WriteLine("Значение функции y = " + y);
+
+
+            //2.3.
+            //int n; // количество учеников в классе
+            //double weight; // вес ученика в кг
+
+            //Console.Write("введите количество учеников в классе: ");
+            //n = Convert.ToInt32(Console.ReadLine());
+
+            //double totalMilk = 0; // общее количество молока
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    Console.Write($"введите вес {i}-го ученика (кг): ");
+            //    weight = Convert.ToDouble(Console.ReadLine());
+
+            //    if (weight < 30)
+            //    {
+            //        // тогда дополнительно выдается стакан молока (0.2 л)
+            //        totalMilk += 0.2; // 0.2 литра молока
+            //    }
+            //}
+
+            //Console.WriteLine($"каждый день для одного класса из {n} учеников нужно {totalMilk} литров молока.");
+            //Console.ReadLine();
+
+
+            //2.6
+            //Console.WriteLine("Введите количество точек:");
+            //int n = int.Parse(Console.ReadLine());
+
+            //int count = 0;
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.WriteLine($"Введите координаты точки {i + 1}:");
+            //    double x = double.Parse(Console.ReadLine());
+            //    double y = double.Parse(Console.ReadLine());
+
+            //    if (x >= 0 && x <= Math.PI && y >= 0 && y <= Math.Sin(x))
+            //    {
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine($"Количество точек, принадлежащих фигуре: {count}");
+
+
+            //2.9
+            //int n = Int32.Parse(Console.ReadLine());
+            //int best = 0;
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    int dist = Int32.Parse(Console.ReadLine());
+            //    best = Math.Max(best, dist);
+            //}
+
+            //Console.WriteLine($"Лучший результат: {best}");
+        }
+    }
+}
