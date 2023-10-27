85751f9025fd6a5d1e926113691968d68780f61a
ZhamsaranovaAyana <146060652+ZhamsaranovaAyana@users.noreply.github.com>
2023-10-27 11:04:15 +0300

    Add files via upload
    лабораторная 2 попытка 2
    добавила 2.9.

---
+++ ayana.cs
@@ -0,0 +1,26 @@
+﻿//2.9.
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("введите количество спортсменов: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int bestResult = int.MaxValue;
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write($"введите результат спортсмена {i}: ");
+            int result = int.Parse(Console.ReadLine());
+
+            if (result < bestResult)
+            {
+                bestResult = result;
+            }
+        }
+
+        Console.WriteLine($"лучший результат спортсмена: {bestResult}");
+    }
+}
